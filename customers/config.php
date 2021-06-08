a8dd395e3496f667453911a48874160dde45ed71
Somanyu <somanyu.03samal@gmail.com>
2021-06-08 09:28:44 +0530

    removed default sql query

--- customers/config.php
+++ customers/config.php
@@ -1,28 +1,21 @@
-<!-- PHP code to establish connection
-with the localserver -->
+<!-- PHP code to establish connection with the database -->
 <?php
 
-// Username is root
+// Username and password(empty)
 $user = 'root';
 $password = '';
 
 // Database name is customers
 $database = 'customers';
 
-// Server is localhost 
 $servername='localhost';
-$mysqli = new mysqli($servername, $user,
-				$password, $database);
+$mysqli = new mysqli($servername, $user, $password, $database);
 
-// Checking for connections
+// Checks for connections
 if ($mysqli->connect_error) {
 	die('Connect Error (' .
 	$mysqli->connect_errno . ') '.
 	$mysqli->connect_error);
 }
 
-// SQL query to select data from database
-$sql = "SELECT * FROM customer_transc";
-$result = $mysqli->query($sql);
-$mysqli->close();
 ?>
