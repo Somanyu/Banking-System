a7e04a1653e797ed4533e74822e8db33dceac5bf
Somanyu <somanyu.03samal@gmail.com>
2021-06-10 15:10:00 +0530

    updated SQL database

--- localhost.session.sql
+++ localhost.session.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.0
+-- version 4.9.5
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Jun 09, 2021 at 07:18 PM
--- Server version: 10.4.18-MariaDB
--- PHP Version: 7.4.16
+-- Host: localhost:3306
+-- Generation Time: Jun 10, 2021 at 07:44 AM
+-- Server version: 10.3.16-MariaDB
+-- PHP Version: 7.3.23
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -18,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `customers`
+-- Database: `id17017226_customers`
 --
 
 -- --------------------------------------------------------
@@ -39,13 +40,13 @@ CREATE TABLE `customer_transc` (
 --
 
 INSERT INTO `customer_transc` (`id`, `full_name`, `email_id`, `balance`) VALUES
-(101, 'Somanyu Samal', 'somanyu.samal@gmail.com', 60000),
-(102, 'Arjun Wagle', 'arjun.wagle@gmail.com', 30992),
-(103, 'Mohini Raj', 'mohini.raj@gmail.com', 44534),
-(104, 'Krishna Sachin', 'krishna.sachin@gmail.com', 19637),
+(101, 'Somanyu Samal', 'somanyu.samal@gmail.com', 48000),
+(102, 'Arjun Wagle', 'arjun.wagle@gmail.com', 30990),
+(103, 'Mohini Raj', 'mohini.raj@gmail.com', 46534),
+(104, 'Krishna Sachin', 'krishna.sachin@gmail.com', 35639),
 (105, 'Rohan Mishra', 'rohan.mishra@gmail.com', 30000),
-(106, 'Gunjan Mehera', 'gunjan.mehra@gmail.com', 44000),
-(107, 'Virat Singh', 'virat.singh@gmail.com', 26000),
+(106, 'Gunjan Mehera', 'gunjan.mehra@gmail.com', 46000),
+(107, 'Virat Singh', 'virat.singh@gmail.com', 31000),
 (108, 'Riya Gada', 'riya.gada@gmail.com', 46003),
 (109, 'Sohan Rajendra', 'rohan.rajendra@gmail.com', 34388),
 (110, 'Priyanka Khandelal', 'priyanka.khandelal@gmail.com', 34000);
@@ -63,6 +64,14 @@ CREATE TABLE `history_transc` (
   `balance` int(11) NOT NULL,
   `datetime` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `history_transc`
+--
+
+INSERT INTO `history_transc` (`serial_no`, `sender`, `receiver`, `balance`, `datetime`) VALUES
+(2, 'Somanyu Samal', 'Virat Singh', 5000, '2021-06-09 22:58:53'),
+(6, 'Arjun Wagle', 'Krishna Sachin', 2, '2021-06-10 08:37:13');
 
 --
 -- Indexes for dumped tables
@@ -88,7 +97,7 @@ ALTER TABLE `history_transc`
 -- AUTO_INCREMENT for table `history_transc`
 --
 ALTER TABLE `history_transc`
-  MODIFY `serial_no` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `serial_no` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
